cd34d03348ffd16d77ca182089a60c318d560e8a
subhankaladi <subhankaladi@gmail.com>
2025-02-26 00:50:49 +0500

    first commit

---
+++ monday-2-5/assignment-1/script.py
@@ -0,0 +1,28 @@
+print("Hello World!")
+
+# Data Types Example
+
+# Integer
+integer_value = 10
+print(integer_value)
+
+# String
+string_value = "Hello, World!"
+print(string_value)
+
+# Float
+float_value = 3.14
+print(float_value)
+
+# Boolean
+bool_value = True
+print(bool_value)
+
+# List
+list_value: str = ["subhan" , "kaladi"]
+print(list_value)
+
+# Dictionary
+dictionary_value = {"name": "Subhan Kaladi", "age": 20}
+print(dictionary_value)
+
