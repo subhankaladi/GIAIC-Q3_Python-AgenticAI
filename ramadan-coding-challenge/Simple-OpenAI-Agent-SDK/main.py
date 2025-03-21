fe5ae37e5fb0424db6f64076e665c99d2a1caa5e
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2025-03-21 07:16:17 -0700

    Update simple agent

--- ramadan-coding-challenge/Simple-OpenAI-Agent-SDK/main.py
+++ ramadan-coding-challenge/Simple-OpenAI-Agent-SDK/main.py
@@ -7,20 +7,26 @@ load_dotenv()
 
 gemini_api_key = os.getenv("GEMINI_API_KEY")
 
-provider = AsyncOpenAI(api_key=gemini_api_key, base_url="https://generativelanguage.googleapis.com/v1beta/openai")
 
-
-model = OpenAIChatCompletionsModel(
-    model="gemini-2.0-flash",
-    openai_client=provider
+provider = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai",
 )
+
+
+model = OpenAIChatCompletionsModel(model="gemini-2.0-flash", openai_client=provider)
 
 agent = Agent(
     name="Greeting Agent",
-    instructions="You are a greeting agent, Your task is to greet user with a friendly message, when someone says hi you've reply back with salam from subhan kaladi, if someone says bye say Allah faiz from Subhan, when someone asks other than says other greeting than say Subhan Kaladi is here just for greeting, nothing else sorrry",
-    model=model
+    instructions="You are a Greeting Agent, Your task is to greet the user with a friendly message, when someone says hi you've reply back with salam from Subhan Kaladi, if someone says bye then say allah hafiz from Subhan kaladi, when someone asks other than greeting then say Subhan is here just for greeting, I can't answer anything else, sorry.",
+    model=model,
 )
-user_question = input("Enter Your Question")
+
+
+user_question = input("Please enter your question: ")
+
+
 result = Runner.run_sync(agent, user_question)
 
 
+print(result.final_output)
---
+++ ramadan-coding-challenge/personal-library-manager/main.py
@@ -0,0 +1,5 @@
+import json
+
+
+class BookCollection:
+    """A class to manage a collection of books, allowing users to store and organize their """
